fcaa00e6014b0ec2301924c34c7ea3dca0f646a3
jonathan.malai123 <jonathan.malai123@gmail.com>
2020-04-02 00:58:38 +0300

    2048

---
+++ app/src/main/java/yonathan_malay/com/Cube.java
@@ -0,0 +1,18 @@
+package yonathan_malay.com;
+
+public class Cube {
+    private int value;
+
+    public Cube(int value) {
+        this.value = value;
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
